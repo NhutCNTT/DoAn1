7b9234b0763e3d3fc6504127dec76374215af3a0
VinhPhuc <vinhphuc123@gmail.com>
2025-09-23 17:45:01 +0700

    Logic Login User

--- src/main/java/Project1/com/LibraryManagement/Controller/UserController.java
+++ src/main/java/Project1/com/LibraryManagement/Controller/UserController.java
@@ -5,6 +5,13 @@ import Project1.com.LibraryManagement.Entity.Users;
 import Project1.com.LibraryManagement.Repository.UserRepos;
 import Project1.com.LibraryManagement.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -21,11 +30,49 @@ public class UserController {
     public UserRepos usersRepos;
     @Autowired
     public UserService userService;
+    @Autowired
+    public PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
     public String login(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken)){
+            return "redirect:/home";  //return home page when login success
+        }
+        return "login"; //when fail return login page
+    }
+
+    @PostMapping("/login")
+    public String processLogin(Model model, Users users){
+        Optional<Users> existsUsers = usersRepos.findByEmail(users.getEmail());
+        if(existsUsers.isPresent()){
+            Users dbUsers = existsUsers.get();
+            if(passwordEncoder.matches(users.getPassword(),dbUsers.getPassword())){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        dbUsers.getEmail(),
+                        dbUsers.getPassword(),
+                        List.of(new SimpleGrantedAuthority("USER"))
+                );
+                //Set Security Context
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                return "redirect:/login";
+
+            }
+            else {
+                model.addAttribute("ErrorLogin","Email Or Password Is Not Correct");
+            }
+
+        }
+        else {
+            model.addAttribute("Error","Email Or Password Is Not Correct");
+        }
         return "login";
     }
+
+
+
+
+
     @GetMapping("/signup")
     public String signup(){
         return "signup";
